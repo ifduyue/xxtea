f063eef41df7419c9b82b8be236c5fc69fbcf965
Yue Du <ifduyue@gmail.com>
2015-01-28 16:02:51 +0800

    More tests

--- tests/test.py
+++ tests/test.py
@@ -1,34 +1,50 @@
-import unittest
-import random
-import xxtea
 import os
+import binascii
+
+import unittest
+import xxtea
 
 
 class TestXXTEA(unittest.TestCase):
 
     def test_hex(self):
-        self.assertEqual(xxtea.encrypt_hex('How do you do?', 'Fine. And you?  '), b'78f465eb1b4985887d11842ede853621')
-        self.assertEqual(xxtea.decrypt_hex('78f465eb1b4985887d11842ede853621', 'Fine. And you?  '), b'How do you do?')
+        data = b'How do you do?'
+        key = b'Fine. And you?  '
+        hexenc = b'78f465eb1b4985887d11842ede853621'
+        self.assertEqual(xxtea.encrypt_hex(data, key), hexenc)
+        self.assertEqual(xxtea.decrypt_hex(hexenc, key), data)
 
     def test_raw(self):
-        encd = xxtea.encrypt('How do you do?', 'Fine. And you?  ')
-        decd = xxtea.decrypt(encd, 'Fine. And you?  ')
-        self.assertEqual(b'How do you do?', decd)
+        data = b'How do you do?'
+        key = b'Fine. And you?  '
+        hexenc = b'78f465eb1b4985887d11842ede853621'
+        enc = binascii.a2b_hex(hexenc)
+
+        self.assertEqual(xxtea.encrypt(data, key), enc)
+        self.assertEqual(xxtea.decrypt(enc, key), data)
 
     def test_urandom(self):
         for i in range(2048):
             key = os.urandom(16)
-
             data = os.urandom(i)
-            encd = xxtea.encrypt(data, key)
-            decd = xxtea.decrypt(encd, key)
-            self.assertEqual(data, decd)
+
+            enc = xxtea.encrypt(data, key)
+            dec = xxtea.decrypt(enc, key)
+            self.assertEqual(data, dec)
 
             data = b'\0' * i
-            encd = xxtea.encrypt(data, key)
-            decd = xxtea.decrypt(encd, key)
-            self.assertEqual(data, decd)
+            enc = xxtea.encrypt(data, key)
+            dec = xxtea.decrypt(enc, key)
+            self.assertEqual(data, dec)
 
+    def test_hex_encode(self):
+        for i in range(2048):
+            key = os.urandom(16)
+            data = os.urandom(i)
+
+            enc = xxtea.encrypt(data, key)
+            hexenc = xxtea.encrypt_hex(data, key)
+            self.assertEqual(binascii.b2a_hex(enc), hexenc)
 
 if __name__ == '__main__':
     unittest.main()
