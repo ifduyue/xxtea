d68cbb35a037f77e4f1662e279351cdad2406b40
Yue Du <ifduyue@gmail.com>
2015-12-10 14:59:21 +0800

    Update bench.py

--- bench.py
+++ bench.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 import sys
 import timeit
@@ -9,14 +10,23 @@ if __name__ == '__main__':
         length = int(sys.argv[1])
         times = int(sys.argv[2])
     except:
-        print >> sys.stderr, 'Usage: {} datalength times'.format(sys.argv[0])
+        print('Usage: {} datalength times'.format(sys.argv[0]), file=sys.stderr)
         sys.exit(-1)
 
     testkey = os.urandom(16)
     testdata = os.urandom(length)
+
     t = timeit.Timer('encrypt({}, {})'.format(repr(testdata), repr(testkey)), 'from xxtea import encrypt')
-    print 'encrypt', t.timeit(times)
+    print('    encrypt:', t.timeit(times))
+
     testdata = xxtea.encrypt(testdata, testkey)
     t = timeit.Timer('decrypt({}, {})'.format(repr(testdata), repr(testkey)), 'from xxtea import decrypt')
-    print 'decrypt', t.timeit(times)
+    print('    decrypt:', t.timeit(times))
+
+    t = timeit.Timer('encrypt_hex({}, {})'.format(repr(testdata), repr(testkey)), 'from xxtea import encrypt_hex')
+    print('encrypt_hex:', t.timeit(times))
+
+    testdata = xxtea.encrypt_hex(testdata, testkey)
+    t = timeit.Timer('decrypt_hex({}, {})'.format(repr(testdata), repr(testkey)), 'from xxtea import decrypt_hex')
+    print('decrypt_hex:', t.timeit(times))
 
