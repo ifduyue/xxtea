47364f94b4f9b8dc80eeae38d23f2e8a151b8051
Yue Du <ifduyue@gmail.com>
2020-01-23 15:48:18 +0800

    Use PyObject_CallMethod instead of PyObject_CallMethodObjArgs

--- xxtea.c
+++ xxtea.c
@@ -43,9 +43,6 @@
 #define MX (((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[(p&3)^e] ^ z)))
 
 static PyObject *module, *binascii;
-static PyObject *_xxtea_pyunicode_hexlify;
-static PyObject *_xxtea_pyunicode_unhexlify;
-static PyObject *_xxtea_pyunicode_decrypt;
 
 static void btea(unsigned int *v, int n, unsigned int const key[4], unsigned int rounds)
 {
@@ -261,7 +258,7 @@ static PyObject *xxtea_encrypt_hex(PyObject *self, PyObject *args, PyObject *kwa
         return NULL;
     }
 
-    retval = PyObject_CallMethodObjArgs(binascii, _xxtea_pyunicode_hexlify, tmp, NULL);
+    retval = PyObject_CallMethod(binascii, "hexlify", "(O)", tmp, NULL);
     Py_DECREF(tmp);
 
     return retval;
@@ -379,11 +376,11 @@ static PyObject *xxtea_decrypt_hex(PyObject *self, PyObject *args, PyObject *kwa
         goto cleanup;
     }
 
-    if (!(tmp = PyObject_CallMethodObjArgs(binascii, _xxtea_pyunicode_unhexlify, data, NULL))) {
+    if (!(tmp = PyObject_CallMethod(binascii, "unhexlify", "(O)", data, NULL))) {
         goto cleanup;
     }
 
-    retval = PyObject_CallMethodObjArgs(module, _xxtea_pyunicode_decrypt, tmp, key, padding, rounds, NULL);
+    retval = PyObject_CallMethod(module, "decrypt", "(OOOO)", tmp, key, padding, rounds, NULL);
     Py_DECREF(tmp);
 
     return retval;
@@ -448,10 +445,6 @@ void initxxtea(void)
         Py_DECREF(module);
         INITERROR;
     }
-
-    _xxtea_pyunicode_hexlify = PyUnicode_FromString("hexlify");
-    _xxtea_pyunicode_unhexlify = PyUnicode_FromString("unhexlify");
-    _xxtea_pyunicode_decrypt = PyUnicode_FromString("decrypt");
 
     PyModule_AddStringConstant(module, "VERSION", VALUE_TO_STRING(VERSION));
 
