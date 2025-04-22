165f21364066ed72def23a24635d4d70890234b8
duyue <ifduyue@gmail.com>
2025-04-22 13:39:15 +0800

    Drop support for Python 3.6

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -35,5 +34,5 @@ setup(
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
