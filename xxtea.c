b98224db6a46fe848f152a892a754b2d883a1a76
Yue Du <ifduyue@gmail.com>
2025-10-02 14:33:56 +0800

    Support Python Free Threading

--- xxtea.c
+++ xxtea.c
@@ -29,7 +29,7 @@
 #include <ctype.h>
 #include <stdio.h>
 
-#define VERSION "3.5.0.dev3"
+#define VERSION "3.5.0"
 
 #if PY_MAJOR_VERSION >= 3
 #define PyString_FromStringAndSize PyBytes_FromStringAndSize
