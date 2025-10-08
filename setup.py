65897e64352c4cb1d567d3b2c20b8d004201c8b5
Yue Du <ifduyue@gmail.com>
2025-10-08 16:15:21 +0800

    Format setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,39 @@
 from setuptools import setup, Extension
-import os
 
-with open('xxtea.c') as f:
+with open("xxtea.c") as f:
     for line in f:
         if line.startswith("#define VERSION "):
             VERSION = eval(line.rsplit(None, 1)[-1])
 
-extension = Extension('xxtea', ['xxtea.c'])
+extension = Extension("xxtea", ["xxtea.c"])
 
 setup(
     name="xxtea",
     version=VERSION,
-    author='Yue Du',
-    author_email='ifduyue@gmail.com',
-    url='https://github.com/ifduyue/xxtea',
+    author="Yue Du",
+    author_email="ifduyue@gmail.com",
+    url="https://github.com/ifduyue/xxtea",
     description="xxtea is a simple block cipher",
-    long_description=open('README.rst', 'rb').read().decode('utf8'),
+    long_description=open("README.rst", "rb").read().decode("utf8"),
     license="BSD",
     keywords="xxtea",
     ext_modules=[extension],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
-        'Programming Language :: Python :: Free Threading :: 1 - Unstable',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "Programming Language :: Python :: Free Threading :: 1 - Unstable",
     ],
     python_requires=">=3.7",
 )
