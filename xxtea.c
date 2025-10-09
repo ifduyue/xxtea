52d87ef51347bc13ebcb09b69845dd41f25b45a9
Yue Du <ifduyue@gmail.com>
2025-10-09 18:36:52 +0800

    Build android wheels

--- xxtea.c
+++ xxtea.c
@@ -29,7 +29,7 @@
 #include <ctype.h>
 #include <stdio.h>
 
-#define VERSION "3.5.1.dev1"
+#define VERSION "3.5.1.dev2"
 
 #if PY_MAJOR_VERSION >= 3
 #define PyString_FromStringAndSize PyBytes_FromStringAndSize
