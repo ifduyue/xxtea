689a0ed55f7e1e264a5ec4e89e071679c780d575
Yue Du <ifduyue@gmail.com>
2026-06-30 15:04:00 +0800

    Release v5.2.1
    - Fix big-endian correctness.
    - Fix longs2bytes in-place handling on big-endian hosts.
    - Add s390x big-endian CI job and regression test.

--- xxtea.c
+++ xxtea.c
@@ -29,7 +29,7 @@
 #include <stdint.h>
 #include <string.h>
 
-#define VERSION "5.2.1.dev0"
+#define VERSION "5.2.1"
 
 #define DELTA 0x9e3779b9U
 #define MX (((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[(p&3)^e] ^ z)))
